6aad6d3f4a438edac44df4b81efd6728e1062394
mohsendroo <mohsendroo@yahoo.com>
2024-07-13 04:53:17 -0700

    Finished Awards page

--- templates/awards.php
+++ templates/awards.php
@@ -1,43 +1,86 @@
 <main>
     <?php include __DIR__ . '/../components/sections/banner3-section.php'; ?>
 
-    <!-- Customer Section Start -->
-    <section id="customer-section" class="container-fluid customer-section">
+    <!-- Anchor Section Start -->
+    <section id="anchor-section" class="container-fluid anchor-section">
         <div class="container">
             <div class="row">
                 <div class="col-12 column">
-                    <div class="title">برخی مشتریان که به ما اعتماد دارند</div>
-                    <div class="logos">
-                        <img src="http://localhost/samim/ui/assets/img/components/customers/Layer_1.png" data-fancybox="logos" alt="">
-                        <img src="http://localhost/samim/ui/assets/img/components/customers/Layer_1-1.png" data-fancybox="logos" alt="">
-                        <img src="http://localhost/samim/ui/assets/img/components/customers/Layer_1-2.png" data-fancybox="logos" alt="">
-                        <img src="http://localhost/samim/ui/assets/img/components/customers/Layer_1-3.png" data-fancybox="logos" alt="">
-                        <img src="http://localhost/samim/ui/assets/img/components/customers/Layer_1-4.png" data-fancybox="logos" alt="">
-                        <img src="http://localhost/samim/ui/assets/img/components/customers/Layer_1-5.png" data-fancybox="logos" alt="">
-                        <img src="http://localhost/samim/ui/assets/img/components/customers/Layer_1.png" data-fancybox="logos" alt="">
-                        <img src="http://localhost/samim/ui/assets/img/components/customers/Layer_1-1.png" data-fancybox="logos" alt="">
-                        <img src="http://localhost/samim/ui/assets/img/components/customers/Layer_1-2.png" data-fancybox="logos" alt="">
-                        <img src="http://localhost/samim/ui/assets/img/components/customers/Layer_1-3.png" data-fancybox="logos" alt="">
-                        <img src="http://localhost/samim/ui/assets/img/components/customers/Layer_1-4.png" data-fancybox="logos" alt="">
-                        <img src="http://localhost/samim/ui/assets/img/components/customers/Layer_1-5.png" data-fancybox="logos" alt="">
-                        <img src="http://localhost/samim/ui/assets/img/components/customers/Layer_1.png" data-fancybox="logos" alt="">
-                        <img src="http://localhost/samim/ui/assets/img/components/customers/Layer_1-1.png" data-fancybox="logos" alt="">
-                        <img src="http://localhost/samim/ui/assets/img/components/customers/Layer_1-2.png" data-fancybox="logos" alt="">
-                        <img src="http://localhost/samim/ui/assets/img/components/customers/Layer_1-3.png" data-fancybox="logos" alt="">
-                        <img src="http://localhost/samim/ui/assets/img/components/customers/Layer_1-4.png" data-fancybox="logos" alt="">
-                        <img src="http://localhost/samim/ui/assets/img/components/customers/Layer_1-5.png" data-fancybox="logos" alt="">
-                        <img src="http://localhost/samim/ui/assets/img/components/customers/Layer_1.png" data-fancybox="logos" alt="">
-                        <img src="http://localhost/samim/ui/assets/img/components/customers/Layer_1-1.png" data-fancybox="logos" alt="">
-                        <img src="http://localhost/samim/ui/assets/img/components/customers/Layer_1-2.png" data-fancybox="logos" alt="">
-                        <img src="http://localhost/samim/ui/assets/img/components/customers/Layer_1-3.png" data-fancybox="logos" alt="">
-                        <img src="http://localhost/samim/ui/assets/img/components/customers/Layer_1-4.png" data-fancybox="logos" alt="">
-                        <img src="http://localhost/samim/ui/assets/img/components/customers/Layer_1-5.png" data-fancybox="logos" alt="">
-                    </div>
+                    <ul class="anchor">
+                        <li class="active"><a href="#awards-section">جوایز و افتخارات</a></li>
+                        <li><a href="#permissions-section">مجوزها</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
     </section>
-    <!-- Customer Section End -->
+    <!-- Anchor Section End -->
+
+    <!-- Awards Section Start -->
+    <section id="awards-section" class="container-fluid awards-section">
+        <div class="row title-row">
+            <div class="col-12 column">
+                <div class="title">گزیده‌ای از جوایز و افتخارات</div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row logos-row">
+                <div class="col-12 column">
+                    <div class="logos">
+                        <img src="http://localhost/samim/ui/assets/img/components/about/image 18.png" data-fancybox="logos" alt="">
+                        <img src="http://localhost/samim/ui/assets/img/components/about/image 19.png" data-fancybox="logos" alt="">
+                        <img src="http://localhost/samim/ui/assets/img/components/about/image 20.png" data-fancybox="logos" alt="">
+                        <img src="http://localhost/samim/ui/assets/img/components/about/image 23.png" data-fancybox="logos" alt="">
+                        <img src="http://localhost/samim/ui/assets/img/components/about/image 18.png" data-fancybox="logos" alt="">
+                        <img src="http://localhost/samim/ui/assets/img/components/about/image 19.png" data-fancybox="logos" alt="">
+                        <img src="http://localhost/samim/ui/assets/img/components/about/image 20.png" data-fancybox="logos" alt="">
+                        <img src="http://localhost/samim/ui/assets/img/components/about/image 23.png" data-fancybox="logos" alt="">
+                    </div>
+                </div>
+            </div>
+
+            <div class="row desc-row">
+                <div class="col-12 column">
+                    <ul class="faq">
+                        <li>انتخاب سیستم جامع اینترکام و سامانه کنترل و مانیتورینگ صمیم به عنوان <span class="important">طرح برتر دانش بنیان</span>، در بخش فنی صنعت رسانه کشور</li>
+                        <li>انتخاب سامانه کنترل و مانیتورینگ صمیم به عنوان <span class="important">محصول برتر تحقیق و توسعه کشور</span> در جشنواره پژوهش و فناوری وزارت صمت</li>
+                        <li>گواهینامه ششمین همایش ملی مدیران فناوری اطلاعات برای سامانه یکپارچه مدیریت دارایی محتوا  iMAM</li>
+                        <li>لوح تقدیر، تندیس و کسب <span class="important">رتبه برتر  شرکت های حوزه فناوری رسانه</span>، دوره های مختلف  اجلاس فناوری رسانه سازمان صدا و سیما</li>
+                        <li>مدال‌های اشتغال دانش بنیان و <span class="important">برند ملی دانش بنیان</span> در نمایشگاه تخصصی شرکت های دانش بنیان</li>
+                        <li>تقديرنامه <span class="important">ارتقاء تکنولوژی در توليد و پخش  و خوداتکایی تجهیزات فنی رسانه</span>، سازمان صدا و سیمای جمهوری اسلامی ایران</li>
+                        <li>موفقیت در طراحی، <span class="important">تولید اینترکام های صنعتی مبتنی بر IP</span> برای اولین بار در کشور</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Awards Section End -->
+
+    <!-- Permissions Section Start -->
+    <section id="permissions-section" class="container-fluid permissions-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 column">
+                    <div class="title">مجوزها</div>
+                    <ul class="faq">
+                        <li>حضور در فهرست برترین شرکت‌های دانش بنیان (تولیدی نوع یک) کشور</li>
+                        <li>گواهی رتبه بندی و احراز صلاحیت شرکت‌های انفورماتیکی از امور نظام فنی و اجرایی سازمان برنامه و بودجه کشور</li>
+                        <li>پروانه بهره برداری صنایع نوین از سازمان صنعت، معدن و تجارت استان تهران</li>
+                        <li>پروانه تحقیق و توسعه، پروانه فنی مهندسی ،جواز تأسیس واحد فنی مهندسی و پروانه تأسیس مرکز پژوهش های صنعتی و معدنی از وزارت صمت</li>
+                        <li>مجوز فعالیت سازمان نظام صنفی رایانه ای کشور</li>
+                        <li>گواهینامه استاندارد مدیریت کیفیت ایـزو 9001:2008 ISO از SGS</li>
+                        <li>عضویت در لیست شرکت‌های پیمانکاری رسته سیستم‌های هوشمند (ITS) معاونت حمل و نقل ترافیک شهرداری تهران</li>
+                        <li>حضور در فهرست تامین کنندگان اصلی مجتمع صنعتی فولاد مبارکه اصفهان</li>
+                        <li>حضور در فهرست تأمین کنندگان اصلی شرکت ملی صنایع مس ایران</li>
+                        <li> حضور در لیست تأمین کنندگان اصلی شرکت ایرانسل و همراه اول</li>
+                        <li>عضویت در سندیکای صنعت مخابرات ایران</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Permissions Section End -->
 
     <!-- Form Section Start -->
     <section id="form-section" class="container-fluid form-section">
